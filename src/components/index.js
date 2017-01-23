6685f5a4d9c0b0960bd12b34706005ac635b84fd
Fredrik Pettersen <fredrik.fumler@gmail.com>
2017-01-23 02:43:57 +0100

    add folder structure, webpack config, package.json etc

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+// webpack magic, finds all the index.js files in subdirs from current path
+const req = require.context('.', true, /\.\/[^/]+\/[^/]+\/index\.js$/)
+
+req.keys().forEach((key) => {
+  const componentName = key.replace(/^.+\/([^/]+)\/index\.js/, '$1')
+  module.exports[componentName] = req(key).default
+})

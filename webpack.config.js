d90d10170a568172147794ffe7c3f12ed2ffac47
Fredrik Pettersen <fredrik.fumler@gmail.com>
2017-01-24 03:03:44 +0100

    the exclude has to be regex, bundle now half the size :fire:

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ const config = {
   ],
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel', exclude: ['/node_modules/', '/src/api/'] }
+      { test: /\.js$/, loader: 'babel', exclude: [/node_modules/, /src\/api/] }
     ]
   }
 }
